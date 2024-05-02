48e88f2f1d361124e221b154541b2d9ce9d14160
sss3322 <suchithrashanmukhan@gmail.com>
2024-05-02 16:46:15 +0530

    First commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,36 @@
+import express from "express";
+import formidable from "express-formidable";
+const router = express.Router();
+
+// controllers
+import {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
+  
+
+} from "../controllers/productController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
+
+router
+  .route("/")
+  .get(fetchProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router.route("/allproducts").get(fetchAllProducts);
+
+
+
+
+router
+  .route("/:id")
+  .get(fetchProductById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizeAdmin, removeProduct);
+
+
+export default router;
